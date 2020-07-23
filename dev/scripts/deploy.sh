f7f61043999dbaa1652278fed6c85f16cf4347a1
Christopher Little <clittle@gsma.com>
2020-07-23 17:18:09 +0100

    Add PoC custom domain record

--- dev/scripts/deploy.sh
+++ dev/scripts/deploy.sh
@@ -3,5 +3,5 @@ sam deploy \
 	--s3-bucket $DEPLOYMENT_BUCKET_NAME-${STAGE:-dev} \
 	--stack-name $STACK_NAME-${STAGE:-dev} \
 	--capabilities CAPABILITY_IAM \
-	--parameter-overrides Stage=${STAGE:-dev} ProjectName=$STACK_NAME DeploymentBucket=$DEPLOYMENT_BUCKET_NAME-${STAGE:-dev} \
+	--parameter-overrides Stage=${STAGE:-dev} ProjectName=$STACK_NAME DeploymentBucket=$DEPLOYMENT_BUCKET_NAME-${STAGE:-dev} DomainName=$DOMAIN_NAME \
 	--no-fail-on-empty-changeset
